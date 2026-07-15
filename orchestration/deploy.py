a277b856b5b26228de93b7026ec2be2dab472326
Alvin Litani <alvinlitani@users.noreply.github.com>
2026-07-15 14:38:13 -0400

    import cron so it will run at specific time

--- orchestration/deploy.py
+++ orchestration/deploy.py
@@ -1,5 +1,6 @@
 from prefect import flow
 from prefect.runner.storage import GitRepository
+from prefect.schedules import Cron
 
 SOURCE = GitRepository(url="https://github.com/alvinlitani/wind-power-forecast.git")
 ENTRYPOINT = "orchestration/trigger.py:trigger_wind_forecast_job"
@@ -9,4 +10,5 @@ if __name__ == "__main__":
         name="daily-trigger",
         work_pool_name="wf-managed",
         job_variables={"pip_packages": ["google-cloud-run"]},
+        schedule=Cron("0 2 * * *", timezone="America/Toronto"),
     )
